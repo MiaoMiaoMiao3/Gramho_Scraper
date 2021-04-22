41dfb7096e3e170acc06635fae46dde2e86e8476
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2021-04-22 08:15:21 -0700

    Added export function

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import scraping_functions as functions
 def main():
 
     retrieved = functions.retrieve_posts(environ.get('client_insta'))
-    print('Subdomains retrieved. Extracting post meta....')
+    # print('Subdomains retrieved. Extracting post meta....')
     
-    post_meta = functions.extract_post_meta(retrieved)
+    post_meta = functions.extract_post_meta([retrieved])
     print('Extraction....COMPLETE')
 
-    functions.export_post_meta(post_meta)
+    # functions.export_post_meta(post_meta)
 
     print('EXPORT COMPLETE! SEE EXPORT.CSV FOR RESULTS')
 
 if __name__ == "__main__":
     main()
+	
--- scraping_functions.py
+++ scraping_functions.py
@@ -18,11 +18,11 @@ def retrieve_posts(client_gramho_url):
 def extract_post_meta(posts):
     # define a class object to hold various attributes for a post
     class Entry:
-        def __init__(self, img_src, total_comments, total_likes, post_date, post_descrip, comments):
-            self.img_src        = img_src
-            self.total_comments = total_comments
-            self.total_likes    = total_likes
+        def __init__(self, post_date, img_src, total_likes, total_comments, post_descrip, comments):
             self.post_date      = post_date
+            self.img_src        = img_src
+            self.total_likes    = total_likes
+            self.total_comments = total_comments
             self.post_descrip   = post_descrip
             self.comments       = comments
 
@@ -32,8 +32,11 @@ def extract_post_meta(posts):
     for post in posts:
         html =requests.get(post).content
         soup = BeautifulSoup(html, 'lxml')
-        print('EXTRACTING URL: ', post)
+        # print('EXTRACTING URL: ', post)
         
+        # EXTRACT the relative date for instagram post --> Use for SQL primary key? (does not cater for duplicate posts on the same day) Maybe date + time
+        post_date = soup.find("div", class_="single-photo-time").get_text()
+
         # EXTRACT post image
         try:
             post_img = soup.find('div', class_='item').find('img')['src']
@@ -57,15 +60,12 @@ def extract_post_meta(posts):
         temp_str = []
         temp_str.append(soup.find("span", class_="icon-chat").get_text())
         temp_str.append(soup.find("span", class_="icon-thumbs-up-alt").get_text())
-        total_comments = temp_str[0][0:temp_str[0].find(" ")]
         total_likes = temp_str[1][0:temp_str[1].find(" ")]
-
-        # EXTRACT the relative date for instagram post --> Use for SQL primary key? (does not cater for duplicate posts on the same day) Maybe date + time
-        post_date = soup.find("div", class_="single-photo-time").get_text()
+        total_comments = temp_str[0][0:temp_str[0].find(" ")]
         
         # EXTRACT post descrip
-        post_descrip = soup.find("div", class_="single-photo-description").get_text().strip()
-
+        post_descrip = soup.find("div", class_="single-photo-description").get_text().replace('•\r\n',' ').strip()
+        print(post_descrip)
         # EXTRACT comments, loop through each comment, store author and comment as array [[user1, comment1], [user2, comment2], ...]
         comments=soup.find_all('div', class_="comment")
         comment_meta = []
@@ -77,8 +77,30 @@ def extract_post_meta(posts):
             meta_temp = [meta_user, meta_comment]
             comment_meta.append(meta_temp)
 
-        posts_meta.append(Entry(post_img, total_comments, total_likes, post_date, post_descrip, comment_meta))
+        posts_meta.append(Entry(post_date, post_img, total_likes, total_comments, post_descrip, comment_meta))
     return posts_meta
 
 def export_post_meta(posts_meta):
     print('exporting post META...')
+    
+    export_CSV = open('EXPORT.CSV', 'w+', encoding="utf8", newline='')
+    
+    print('writing to CSV.....')
+    export_CSV.write('DATE| IMG_SRC| LIKES| COMMENTS| DESCRIP|n')
+    for post in posts_meta:
+        
+        export_CSV.write(post.post_date)
+        export_CSV.write('|')
+        export_CSV.write(post.img_src)
+        export_CSV.write('|')
+        export_CSV.write(post.total_likes)
+        export_CSV.write('|')
+        export_CSV.write(post.total_comments)
+        export_CSV.write('|')
+        export_CSV.write(post.post_descrip)
+        export_CSV.write('|')
+        # export_CSV.write(post.comments)
+        export_CSV.write('\n\n')
+        
+
+    export_CSV.close()
