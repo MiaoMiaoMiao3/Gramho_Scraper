1dfc84485ce0fbb002171da29b7b641982e958ae
MiaoMiaoMiao3 <uyen.a.tran@gmail.com>
2021-04-22 13:38:20 -0700

    Added scrape functions to cleanse extracted data

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import scraping_functions as functions
 def main():
 
     retrieved = functions.retrieve_posts(environ.get('client_insta'))
-    # print('Subdomains retrieved. Extracting post meta....')
+    print('Subdomains retrieved. Extracting post meta....\n')
     
-    post_meta = functions.extract_post_meta([retrieved])
-    print('Extraction....COMPLETE')
+    post_meta = functions.extract_post_meta(retrieved)
+    print('Extraction....COMPLETE\n')
 
-    # functions.export_post_meta(post_meta)
+    functions.export_post_meta(post_meta)
 
-    print('EXPORT COMPLETE! SEE EXPORT.CSV FOR RESULTS')
+    print('EXPORT COMPLETE! See EXPORT.CSV for')
 
 if __name__ == "__main__":
     main()
--- scraping_functions.py
+++ scraping_functions.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-
+# TODO: move retrieve posts and export posts to separate files
+# TODO: Create a more customizeable way for the user to specifiy what data to export and
+# if and how data should be cleansed.
 
 def retrieve_posts(client_gramho_url):
     # retrieve all subdomain links from instagram account 
@@ -18,15 +20,17 @@ def retrieve_posts(client_gramho_url):
 def extract_post_meta(posts):
     # define a class object to hold various attributes for a post
     class Entry:
-        def __init__(self, post_date, img_src, total_likes, total_comments, post_descrip, comments):
+        def __init__(self, post_date, img_src, total_likes, total_comments, hashes_cnt, comments, post_descrip):
             self.post_date      = post_date
             self.img_src        = img_src
             self.total_likes    = total_likes
             self.total_comments = total_comments
+            self.hashes_cnt     = hashes_cnt
             self.post_descrip   = post_descrip
             self.comments       = comments
 
     posts_meta = []
+    hash_count = {}
 
     # loop through each post and extract information
     for post in posts:
@@ -63,9 +67,11 @@ def extract_post_meta(posts):
         total_likes = temp_str[1][0:temp_str[1].find(" ")]
         total_comments = temp_str[0][0:temp_str[0].find(" ")]
         
-        # EXTRACT post descrip
-        post_descrip = soup.find("div", class_="single-photo-description").get_text().replace('•\r\n',' ').strip()
-        print(post_descrip)
+        # EXTRACT post descrip -- from this we determined the hash count
+        post_descrip = soup.find("div", class_="single-photo-description")
+        descrip_hash = cleanse_text_only(post_descrip.find_all('a'))
+        dictionary_count(hash_count, descrip_hash)
+
         # EXTRACT comments, loop through each comment, store author and comment as array [[user1, comment1], [user2, comment2], ...]
         comments=soup.find_all('div', class_="comment")
         comment_meta = []
@@ -74,19 +80,33 @@ def extract_post_meta(posts):
             meta_user = comment.find('div', class_="comment-user").get_text().strip()
             meta_comment = comment.find('div', class_="comment-text").get_text().strip()
         
-            meta_temp = [meta_user, meta_comment]
-            comment_meta.append(meta_temp)
+            comment_meta.append(meta_comment)
 
-        posts_meta.append(Entry(post_date, post_img, total_likes, total_comments, post_descrip, comment_meta))
+        posts_meta.append(Entry(post_date, post_img, total_likes, total_comments, hash_count, comment_meta, post_descrip.get_text().strip()))
     return posts_meta
 
+
+def cleanse_text_only(dirty_array):
+    cleaned_array = dirty_array
+    for i in range(0,len(cleaned_array)):
+        cleaned_array[i] = cleaned_array[i].get_text()
+        
+    return (cleaned_array)
+
+def dictionary_count(k, v):
+    for value in v:
+        try:
+            k[value]+=1
+        except:
+            k[value] = 1
+
 def export_post_meta(posts_meta):
-    print('exporting post META...')
+    print('Exporting post META...')
     
     export_CSV = open('EXPORT.CSV', 'w+', encoding="utf8", newline='')
     
     print('writing to CSV.....')
-    export_CSV.write('DATE| IMG_SRC| LIKES| COMMENTS| DESCRIP|n')
+    export_CSV.write('DATE| IMG_SRC| LIKES| COMMENTS_CNT| COMMENTS| DESCRIP_LF |\n')
     for post in posts_meta:
         
         export_CSV.write(post.post_date)
@@ -97,10 +117,12 @@ def export_post_meta(posts_meta):
         export_CSV.write('|')
         export_CSV.write(post.total_comments)
         export_CSV.write('|')
-        export_CSV.write(post.post_descrip)
-        export_CSV.write('|')
-        # export_CSV.write(post.comments)
+        export_CSV.write(str(post.comments))
+        export_CSV.write('\n')
+        export_CSV.write(str(post.post_descrip))
         export_CSV.write('\n\n')
-        
-
+    
+    export_CSV.write('HASH COUNT')
+    export_CSV.write(str(post.hashes_cnt))
+    export_CSV.write('\n')
     export_CSV.close()
